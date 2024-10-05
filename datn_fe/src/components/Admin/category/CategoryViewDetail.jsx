459d76e6d179bd63f0305527117e39e0e7d38850
minh692 <nguyennhatminh06092002@gmail.com>
2024-10-05 13:30:21 +0700

    Update CategoryViewDetail FE

--- datn_fe/src/components/Admin/category/CategoryViewDetail.jsx
+++ datn_fe/src/components/Admin/category/CategoryViewDetail.jsx
@@ -1,34 +1,28 @@
-// import { Avatar,  Descriptions, Drawer, Modal } from "antd";
+// import { Avatar, Badge, Descriptions, Drawer, Modal } from "antd";
 // import moment from 'moment';
 // import { AntDesignOutlined } from "@ant-design/icons";
 // import { useState } from 'react';
 //
-// const CategoryViewDetail = (props) => {
-//     // eslint-disable-next-line react/prop-types
+// const UserViewDetail = (props) => {
 //     const { openViewDetail, setOpenViewDetail, dataViewDetail, setDataViewDetail } = props;
 //
 //     // State để kiểm soát modal hiển thị ảnh lớn
 //     const [isModalOpen, setIsModalOpen] = useState(false);
-//     const [modalImageUrl, setModalImageUrl] = useState(null);
 //
 //     const onClose = () => {
 //         setOpenViewDetail(false);
 //         setDataViewDetail(null);
 //     }
 //
-//     // const handleAvatarClick = () => {
-//     //     setIsModalOpen(true);
-//     // };
-//
-//     const handleThumbnailClick = (thumbnailUrl) => {
-//         setModalImageUrl(thumbnailUrl);
+//     const handleAvatarClick = () => {
 //         setIsModalOpen(true);
 //     };
+//
 //     const handleModalClose = () => {
 //         setIsModalOpen(false);
 //     };
-//     // Đảm bảo URL đầy đủ cho thumbnail (cập nhật với đường dẫn đúng)
-//     const urlThumbnail = `${import.meta.env.VITE_BACKEND_URL}/storage/${dataViewDetail?.thumbnail}`;
+//
+//     const urlAvatar = `${import.meta.env.VITE_BACKEND_URL}/storage/category-thumbnail/${dataViewDetail?.thumbnail}`;
 //
 //     return (
 //         <>
@@ -42,25 +36,21 @@
 //                     bordered
 //                     column={2}
 //                 >
-//
-//                     <Descriptions.Item label="Id">{dataViewDetail?.id}</Descriptions.Item>
-//                     <Descriptions.Item label="Name">{dataViewDetail?.name}</Descriptions.Item>
-//                     <Descriptions.Item label="Thumbnail">
-//                         {/* Hiển thị ảnh thu nhỏ và cho phép click để phóng to */}
-//                         <img
-//                             src={urlThumbnail}
-//
-//                             alt="thumbnail"
-//                             style={{ width: '100px', cursor: 'pointer' }}
-//                             onClick={() => handleThumbnailClick(dataViewDetail?.thumbnail)}
+//                     <Descriptions.Item label="Thumbnail" span={2}>
+//                         <Avatar
+//                             size={100}
+//                             icon={<AntDesignOutlined />}
+//                             src={urlAvatar}
+//                             shape="circle"
+//                             style={{ boxShadow: '0 2px 8px rgba(0,0,0,0.15)', margin: '0 auto', display: 'block', cursor: 'pointer' }}
+//                             onClick={handleAvatarClick} // Thêm sự kiện click để mở modal
 //                         />
 //                     </Descriptions.Item>
+//                     <Descriptions.Item label="Id">{dataViewDetail?.id}</Descriptions.Item>
+//                     <Descriptions.Item label="Name">{dataViewDetail?.name}</Descriptions.Item>
 //                     <Descriptions.Item label="Description">{dataViewDetail?.description}</Descriptions.Item>
 //                     <Descriptions.Item label="Hot">
 //                         {dataViewDetail?.hot ? 'Yes' : 'No'}
-//                     </Descriptions.Item>
-//                     <Descriptions.Item label="Active">
-//                         {dataViewDetail?.active ? 'Actived' : 'NotActive'}
 //                     </Descriptions.Item>
 //                     <Descriptions.Item label="Created At">
 //                         {moment(dataViewDetail?.createdAt).format('DD-MM-YYYY hh:mm:ss')}
@@ -84,8 +74,8 @@
 //                 footer={null}
 //             >
 //                 <img
-//                     src={modalImageUrl}
-//                     alt="Thumbnail"
+//                     src={urlAvatar}
+//                     alt="Avatar"
 //                     style={{ width: '100%' }}
 //                 />
 //             </Modal>
@@ -93,38 +83,61 @@
 //     );
 // }
 //
-// export default CategoryViewDetail;
-import { Avatar, Badge, Descriptions, Drawer, Modal } from "antd";
+// export default UserViewDetail;
+
+import {Avatar, Badge, Descriptions, Divider, Drawer, Modal, Upload} from "antd";
 import moment from 'moment';
 import { AntDesignOutlined } from "@ant-design/icons";
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-const UserViewDetail = (props) => {
+const CategoryViewDetail = (props) => {
     const { openViewDetail, setOpenViewDetail, dataViewDetail, setDataViewDetail } = props;
-
-    // State để kiểm soát modal hiển thị ảnh lớn
-    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const onClose = () => {
         setOpenViewDetail(false);
         setDataViewDetail(null);
     }
 
-    const handleAvatarClick = () => {
-        setIsModalOpen(true);
+    const [previewOpen, setPreviewOpen] = useState(false);
+    const [previewImage, setPreviewImage] = useState('');
+    const [previewTitle, setPreviewTitle] = useState('');
+
+    const [fileList, setFileList] = useState([]);
+
+    useEffect(() => {
+        if (dataViewDetail) {
+            let imgThumbnail = {};
+            if (dataViewDetail.thumbnail) {
+                imgThumbnail = {
+                    uid: uuidv4(),
+                    name: dataViewDetail.thumbnail,
+                    status: 'done',
+                    url: `${import.meta.env.VITE_BACKEND_URL}/storage/category-thumbnail/${dataViewDetail.thumbnail}`,
+                }
+            }
+
+            setFileList([imgThumbnail])
+        }
+    }, [dataViewDetail])
+
+    const handleCancel = () => setPreviewOpen(false);
+
+    const handlePreview = async (file) => {
+        setPreviewImage(file.url);
+        setPreviewOpen(true);
+        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
     };
 
-    const handleModalClose = () => {
-        setIsModalOpen(false);
-    };
-
-    const urlAvatar = `${import.meta.env.VITE_BACKEND_URL}/storage/category-thumbnail/${dataViewDetail?.thumbnail}`;
+    const handleChange = ({ fileList: newFileList }) => {
+        setFileList(newFileList);
+    }
 
     return (
         <>
             <Drawer
                 title="Category Detail"
-                width={"60vw"}
+                width={"70vw"}
                 onClose={onClose}
                 open={openViewDetail}
             >
@@ -132,16 +145,6 @@ const UserViewDetail = (props) => {
                     bordered
                     column={2}
                 >
-                    <Descriptions.Item label="Thumbnail" span={2}>
-                        <Avatar
-                            size={100}
-                            icon={<AntDesignOutlined />}
-                            src={urlAvatar}
-                            shape="circle"
-                            style={{ boxShadow: '0 2px 8px rgba(0,0,0,0.15)', margin: '0 auto', display: 'block', cursor: 'pointer' }}
-                            onClick={handleAvatarClick} // Thêm sự kiện click để mở modal
-                        />
-                    </Descriptions.Item>
                     <Descriptions.Item label="Id">{dataViewDetail?.id}</Descriptions.Item>
                     <Descriptions.Item label="Name">{dataViewDetail?.name}</Descriptions.Item>
                     <Descriptions.Item label="Description">{dataViewDetail?.description}</Descriptions.Item>
@@ -161,22 +164,27 @@ const UserViewDetail = (props) => {
                         {dataViewDetail?.updatedBy}
                     </Descriptions.Item>
                 </Descriptions>
+                <Divider orientation="left" > Category Thumbnail </Divider>
+
+                <Upload
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                    onChange={handleChange}
+                    showUploadList={
+                        { showRemoveIcon: false }
+                    }
+                >
+
+                </Upload>
+                <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel} centered>
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
             </Drawer>
 
-            {/* Modal hiển thị ảnh lớn */}
-            <Modal
-                open={isModalOpen}
-                onCancel={handleModalClose}
-                footer={null}
-            >
-                <img
-                    src={urlAvatar}
-                    alt="Avatar"
-                    style={{ width: '100%' }}
-                />
-            </Modal>
         </>
     );
 }
 
-export default UserViewDetail;
+export default CategoryViewDetail;
