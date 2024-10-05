79760107bcfd45f1293cb4377e65aa39e3cfe5af
minh692 <nguyennhatminh06092002@gmail.com>
2024-10-05 17:22:38 +0700

    Fix FE

---
+++ datn_fe/src/pages/admin/category/index.jsx
@@ -0,0 +1,13 @@
+
+import CategoryTable from "../../../components/Admin/category/CategoryTable.jsx";
+
+const ManageCategoryPage = () => {
+    return(
+        <div className="manage-category-page">
+            <CategoryTable/>
+        </div>
+    )
+
+}
+
+export default ManageCategoryPage;
