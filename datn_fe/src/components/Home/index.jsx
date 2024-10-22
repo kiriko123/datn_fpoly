ecfec216016edb989fb00d610e038b7483be725c
Tan Khang <tankhang101a@gmail.com>
2024-10-22 20:11:05 +0700

    init PayPal

--- datn_fe/src/components/Home/index.jsx
+++ datn_fe/src/components/Home/index.jsx
@@ -3,10 +3,20 @@ import ImageLayout from "../Home/ImageLayout/ImageLayout.jsx";
 import HotProducts from "../Home/HotProducts/HotProducts.jsx";
 import HotCategories from "../Home/HotCategory/HotCategories.jsx";
 import BrandList from "./BrandList.jsx";
+// import Checkout from "./Checkout.jsx";
+// import {PayPalScriptProvider} from "@paypal/react-paypal-js";
+// const initialOptions = {
+//     "client-id": "AXPtAiVzwdwajXcuJ3K0dM8gqb_6y67vGfuGxnoUGNwryqD8JYGe1J7KpN29iUQy0JbZU5BYajlI8n9k",
+//     currency: "USD",
+//     intent: "capture",
+// };
 
 const Home = () => {
     return (
         <>
+            {/*<PayPalScriptProvider options={initialOptions}>*/}
+            {/*    <Checkout />*/}
+            {/*</PayPalScriptProvider>*/}
             <Slider />
             <BrandList/>
             <HotProducts />
